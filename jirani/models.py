b63a35e3b41e89ac88f642476bfa88a91a3ad543
Sam Oluoch <samsoluoch@gmail.com>
2018-10-16 21:35:27 +0300

    created Post model and defined the methods that will be applied to it

--- jirani/models.py
+++ jirani/models.py
@@ -35,3 +35,68 @@ class Profile(models.Model):
     def filter_by_id(cls, id):
         profile = Profile.objects.filter(user=id).first()
         return profile
+
+
+
+
+class Post(models.Model):
+    '''
+    This is image class model
+    '''
+    image = models.ImageField(upload_to='image/', null=True)
+    name = models.CharField(max_length =60)
+    description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location, null=True)
+
+    def save_post(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    # @classmethod
+    # def todays_image(cls):
+    #     today = dt.date.today()
+    #     image = cls.objects.filter(pub_date__date=today)
+    #     return image
+
+    @classmethod
+    def search_by_category(cls, search_term):
+        # cat = category.objects.get(name=search_term)
+        posts = cls.objects.filter(category__name__icontains=search_term)
+        return posts
+
+    @classmethod
+    def search_by_location(cls, search_term):
+        posts = cls.objects.filter(location__name__icontains=search_term)
+        return posts
+
+    # @classmethod
+    # def days_image(cls, date):
+    #     image = cls.objects.filter(pub_date__date=date)
+    #     return image
+
+    @classmethod
+    def all_posts(cls):
+        posts = cls.objects.all()
+        return posts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
