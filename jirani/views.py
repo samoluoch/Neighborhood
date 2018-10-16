51477cfdd07699ecdd3a6594d3340e5d537c40ff
Sam Oluoch <samsoluoch@gmail.com>
2018-10-16 15:59:43 +0300

    created registration view function

--- jirani/views.py
+++ jirani/views.py
@@ -11,6 +11,38 @@ def home(request):
     return render(request,'home.html')
 
 
+
+def register(request):
+    if request.user.is_authenticated():
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            form = RegistrationForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                # user.is_active = False
+                user.save()
+                # current_site = get_current_site(request)
+                # to_email = form.cleaned_data.get('email')
+                # activation_email(user, current_site, to_email)
+                # return HttpResponse('Please confirm your email')
+            return redirect('home.html')
+
+        else:
+            form = RegistrationForm()
+        return render(request, 'registration/signup.html',{'form':form})
+
+
+
+
+
+
+
+
+
+
+
+
 def search_profile(request):
     if 'profile' in request.GET and request.GET['profile']:
         search_term = request.GET.get('profile')
