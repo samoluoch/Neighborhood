6d2ed9484bbd16cf172c54f49c723128b5be91b4
Sam Oluoch <samsoluoch@gmail.com>
2018-10-16 16:05:56 +0300

    created signup.html and passed in the registration form. Also added registration url to the urls.py

--- jirani/urls.py
+++ jirani/urls.py
@@ -12,5 +12,6 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$', views.home, name='home'),
     url(r'^search/$', views.search_profile, name='search_project'),
+    url(r'^signup/', views.register, name='signup'),
 
 ]
