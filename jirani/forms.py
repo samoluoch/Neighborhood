3b7d7bf1dd1cbc1fe4dcb8c1111ca9c8a7f5edfe
Sam Oluoch <samsoluoch@gmail.com>
2018-10-16 16:58:58 +0300

    created the edit profile form

--- jirani/forms.py
+++ jirani/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
+
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text = 'Required')
@@ -8,3 +10,16 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name','last_name', 'username', 'email', 'password1', 'password2')
+
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+
+
+
+
+
+
